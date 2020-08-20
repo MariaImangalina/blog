c528687f6156a7a86467a617d291e4d92596ef2a
geo_varenik <varenik_geo@mail.ru>
2020-08-20 14:38:42 +0300

    add LoginView and LogoutView. It works!

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import login, logout
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myblog.urls')),
-    path('account/login/', login, name='login'),
-    path('account/logout/', logout, name='logout', kwargs={'next_page':'/'}),
+    path('account/login/', auth_views.LoginView.as_view(), name='login'),
+    path('account/logout/', auth_views.LogoutView.as_view(), name='logout', kwargs={'next_page':'/'}),
 ]
